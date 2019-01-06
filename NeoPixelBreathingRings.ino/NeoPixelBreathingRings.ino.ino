8afa7a6b87408043056377f8aa7c7a98b26beba4
Vincent van Beek <vin.van.beek@outlook.com>
2019-01-06 23:01:48 +0100

    added NeoPixel-Breathing-Rings script

---
+++ NeoPixelBreathingRings.ino/NeoPixelBreathingRings.ino.ino
@@ -0,0 +1,101 @@
+// NeoPixel Stair Led-strips
+// Simple example by: Vincent van Beek
+// 06-01-2019
+
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+#include <avr/power.h>
+#endif
+
+
+// Which pin on the Arduino is connected to the NeoPixels?
+#define PIN             6        // Pin 6 used for the NeoPixels
+
+// How many NeoPixels are attached to the Arduino?
+#define NUMBEROFRING      2       // Number of (equal size) NeoPixel rings
+#define LEDSPERRING       60      // Number of Pixels per ring
+#define NUMPIXELS         NUMBEROFRING*LEDSPERRING 
+
+
+Adafruit_NeoPixel neoPixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+// Set up Variables
+unsigned long timeLoopStart=0;    // timestamp to remember when loop has started. used to determine the end-delay (keeps loop running in same intervals)
+unsigned long timeTemp=0;         // temp. var used in time calculations
+unsigned long timeDiff=0;         // temp. var used in time calculations
+
+
+//-------------------------------------------------------------------------
+// Tuning part for the Breathe effect en turn-on and turn-off speed 
+//-------------------------------------------------------------------------
+int change = 1;                   // used in 'breathing' the LED's , make value smalle to make it smoother, or higher to make it faster
+int breathe = 15;                 // used in 'breathing' the LED's.
+
+
+void setup() {
+  neoPixels.begin();            // This initializes the NeoPixel library.
+  neoPixels.setBrightness(35);  // Adjust brightness here
+  clearStrips();                // Initialize all pixels to 'off', and do neoPixels.show()
+  delay (100);
+}
+
+ 
+void loop() {
+
+  // register the current time (in msec, for later use to optimize the execution loop)
+  timeLoopStart=millis();
+  
+  handleBreathe();        // Then Enable the cool "breathe effect" of the led strip lights
+}
+
+
+void handleBreathe() {
+
+  // blue value changes
+  breathe = breathe + change;
+  
+  // breathe the LED from 15 = off to 80 = fairly bright, change values if needed
+  if ( (breathe >= 80 || breathe <= 15) ) {
+  
+    change = -change;         // toggle the value to increase/decrease the breathe value
+  
+    timeTemp = millis();
+    timeDiff = timeTemp - timeLoopStart;
+  
+    // Program execution was shorter than 150 msec ? Then delay up to 150 msec, for a smooth breathe
+    // NOTE: shorten the 150-numbers to a lower (same) value when breath is to slow..
+    if (timeDiff < 300) {
+      delay(300-timeDiff);  // Pause at beginning and end of each breath
+    }
+  }
+  else {
+
+    // All pixels "breathe"
+    for (int l=0; l<NUMPIXELS; l++){
+      neoPixels.setPixelColor(l, neoPixels.Color(0,0,breathe));
+    }
+
+    neoPixels.show(); // Update the NeoPixel ring
+
+
+    timeTemp = millis();
+    timeDiff = timeTemp - timeLoopStart;
+  
+    // Program execution (timeDiff) was shorter than 25 msec ? Then delay up to 25 msec, for a smooth breathe
+    // NOTE: shorten the 25-numbers to a lower (same) value when breath is to slow..
+    if (timeDiff < 20) {
+      delay(20-timeDiff);
+    }
+  }
+}
+
+
+void clearStrips(){
+  // All pixels off
+  for (int l=0; l<NUMPIXELS; l++){
+    neoPixels.setPixelColor(l, neoPixels.Color(0,0,0));
+  }
+
+  neoPixels.show(); // This sends the updated pixel's to the hardware.  
+}
+
--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-# NeoPixel-Stair
+# NeoPixel-Stair & NeoPixel-Breathing-Rings
+2 Arduino programs for the NeoPixel led strips and rings.
+
+## NeoPixel-Stair - functions
 Stair LedLights, using NeoPixel Ledstrips and an Arduino.
 
-## Functions:
 - drives a configurable number of led-strips (one ledstrip per step)
 - with configurable number of leds per strip
 - has a nice "breathe" function when stairs are not used (smoothly up and down of first and last led of each led-strip ; configurable also)
@@ -32,4 +34,19 @@ int turnOnSpeed=250;              // speed to turn on next led-strip, in msec be
 int turnOffSpeed=100;             // speed to turn on next led-strip, in msec between next strip
 int keepLedsOnTime=18000;         // keep leds on for at least .. msec.
 int keepLedsOffTime=1500;         // keep leds off for at least .. msec.
+```
+
+
+## NeoPixel-Breathing-Rings - functions
+Just a cool script to smoothly brighten and soften the leds in a nice color.
+I 2 NeoPixel rings with 60 leds each and used a blue calm color... but change it to your needs of course...
+
+```javascript
+// Which pin on the Arduino is connected to the NeoPixels?
+#define PIN             6        // Pin 6 used for the NeoPixels
+
+// How many NeoPixels are attached to the Arduino?
+#define NUMBEROFRING      2       // Number of (equal size) NeoPixel rings
+#define LEDSPERRING       60      // Number of Pixels per ring
+#define NUMPIXELS         NUMBEROFRING*LEDSPERRING 
 ```
