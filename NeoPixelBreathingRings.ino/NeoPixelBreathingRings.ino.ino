baa98120e13116f4d5d08832fd64baf42b3da24f
Vincent van Beek <vin.van.beek@outlook.com>
2019-01-06 23:11:01 +0100

    more brightness and smoother

--- NeoPixelBreathingRings.ino/NeoPixelBreathingRings.ino.ino
+++ NeoPixelBreathingRings.ino/NeoPixelBreathingRings.ino.ino
@@ -9,7 +9,7 @@
 
 
 // Which pin on the Arduino is connected to the NeoPixels?
-#define PIN             6        // Pin 6 used for the NeoPixels
+#define PIN               6       // Pin 6 used for the NeoPixels
 
 // How many NeoPixels are attached to the Arduino?
 #define NUMBEROFRING      2       // Number of (equal size) NeoPixel rings
@@ -29,13 +29,13 @@ unsigned long timeDiff=0;         // temp. var used in time calculations
 // Tuning part for the Breathe effect en turn-on and turn-off speed 
 //-------------------------------------------------------------------------
 int change = 1;                   // used in 'breathing' the LED's , make value smalle to make it smoother, or higher to make it faster
-int breathe = 15;                 // used in 'breathing' the LED's.
+int breathe = 10;                 // used in 'breathing' the LED's.
 
 
 void setup() {
-  neoPixels.begin();            // This initializes the NeoPixel library.
-  neoPixels.setBrightness(35);  // Adjust brightness here
-  clearStrips();                // Initialize all pixels to 'off', and do neoPixels.show()
+  neoPixels.begin();              // This initializes the NeoPixel library.
+  //neoPixels.setBrightness(35);  // Adjust brightness here
+  clearStrips();                  // Initialize all pixels to 'off', and do neoPixels.show()
   delay (100);
 }
 
@@ -45,7 +45,7 @@ void loop() {
   // register the current time (in msec, for later use to optimize the execution loop)
   timeLoopStart=millis();
   
-  handleBreathe();        // Then Enable the cool "breathe effect" of the led strip lights
+  handleBreathe();        // Enable the cool "breathe effect" of the led strip lights
 }
 
 
@@ -54,18 +54,18 @@ void handleBreathe() {
   // blue value changes
   breathe = breathe + change;
   
-  // breathe the LED from 15 = off to 80 = fairly bright, change values if needed
-  if ( (breathe >= 80 || breathe <= 15) ) {
+  // breathe the LED from 10 = off to 80 = fairly bright, change values if needed
+  if ( (breathe >= 80 || breathe <= 10) ) {
   
     change = -change;         // toggle the value to increase/decrease the breathe value
   
     timeTemp = millis();
     timeDiff = timeTemp - timeLoopStart;
   
-    // Program execution was shorter than 150 msec ? Then delay up to 150 msec, for a smooth breathe
-    // NOTE: shorten the 150-numbers to a lower (same) value when breath is to slow..
-    if (timeDiff < 300) {
-      delay(300-timeDiff);  // Pause at beginning and end of each breath
+    // Program execution was shorter than 250 msec ? Then delay up to 250 msec, for a smooth breathe
+    // NOTE: shorten the 250-numbers to a lower (same) value when breath is to slow..
+    if (timeDiff < 250) {
+      delay(250-timeDiff);  // Pause at beginning and end of each breath
     }
   }
   else {
@@ -81,8 +81,8 @@ void handleBreathe() {
     timeTemp = millis();
     timeDiff = timeTemp - timeLoopStart;
   
-    // Program execution (timeDiff) was shorter than 25 msec ? Then delay up to 25 msec, for a smooth breathe
-    // NOTE: shorten the 25-numbers to a lower (same) value when breath is to slow..
+    // Program execution (timeDiff) was shorter than .. msec ? Then delay up to .. msec, for a smooth breathe
+    // NOTE: shorten the numbers to a lower (same) value when breath is to slow..
     if (timeDiff < 20) {
       delay(20-timeDiff);
     }
