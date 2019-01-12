443bc544d0e285108b7c124d893fe42a16656a9b
Vincent van Beek <vin.van.beek@outlook.com>
2019-01-12 23:04:00 +0100

    LDR first version / ready for test

--- NeoPixelStairs.ino/NeoPixelStairs.ino.ino
+++ NeoPixelStairs.ino/NeoPixelStairs.ino.ino
@@ -8,13 +8,42 @@
 #endif
 
 
-// Which pin on the Arduino is connected to the NeoPixels?
-#define PIN             5        // Pin 5 used for the NeoPixels
+// Configuration of the NeoPixel output
+#define PIN             5        // Pin used for the NeoPixels
 
-// How many NeoPixels are attached to the Arduino?
-#define LEDSPERSTRIP    36       // 36 leds per strip
-#define LEDSTRIPS       16       // 16 steps
+// How many NeoPixels are attached to the Arduino ?
+#define LEDSPERSTRIP    36       // Number of leds per strip
+#define LEDSTRIPS       16       // Number of stair steps (led strips)
 #define NUMPIXELS       LEDSTRIPS*LEDSPERSTRIP 
+
+// Configuration of the Passive Infrared (PIR)
+int alarmPinTop = 10;             // PIR at the top of the stairs
+int alarmPinBottom = 11;          // PIR at the bottom of the stairs
+int alarmValueTop = HIGH;         // Variable to hold the PIR status
+int alarmValueBottom = HIGH;      // Variable to hold the PIR status
+
+// Configuration of the Light dependent resistor (LDR)
+bool useLDR = true;               // flag, when true the program uses the LDR, set to "false" if you don't have a LDR sensor.
+int LDRSensor = A0;               // Light dependent resistor, Analog Input line 
+int LDRValue = 0;                 // Variable to hold the measured LDR value
+int LDRThreshold = 600;           // Only switch on LED's at night when LDR senses low light conditions - you may have to change this value for your circumstances!
+
+// Other Input/output
+int ledPin = 13;                  // LED on the arduino board flashes when PIR activated
+
+// Set up Variables for the needed program logic (DO NOT TOUCH THESE!)
+unsigned long timeOut = 0;        // timestamp to remember when the PIR was triggered.
+unsigned long timeLoopStart = 0;  // timestamp to remember when loop has started. used to determine the end-delay (keeps loop running in same intervals)
+unsigned long timeTemp = 0;       // temp. var used in time calculations
+unsigned long timeDiff = 0;       // temp. var used in time calculations
+bool readPIRInputs = true;        // flag, when true, reads the PIR sensors. Disabled (false) by the program when LDR indicated that there is enough light.
+int downUp = 0;                   // main program mode, The possible values are:
+                                  //  0   =   Idle mode, waiting for PIR trigger to turn stairs on.
+                                  //  1   =   Going down, Turning stairs on (direction up to down)
+                                  //  2   =   Going up, Turnurning stairs on (direction down to up)
+                                  //  3   =   Turning leds off, from top to down
+                                  //  4   =   Turninf leds off, from bottom to up
+                                  //  5   =   All leds just turned off. After short time, automatically mode will be set to "0".
 
 
 // When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
@@ -22,30 +51,18 @@
 // example for more information on possible values.
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
-// Set up Variables
-unsigned long timeOut=0;          // timestamp to remember when the PIR was triggered.
-unsigned long timeLoopStart=0;    // timestamp to remember when loop has started. used to determine the end-delay (keeps loop running in same intervals)
-unsigned long timeTemp=0;         // temp. var used in time calculations
-unsigned long timeDiff=0;         // temp. var used in time calculations
-int downUp = 0;                   // variable to rememer the direction of travel up or down the stairs
-int alarmPinTop = 10;             // PIR at the top of the stairs
-int alarmPinBottom =11;           // PIR at the bottom of the stairs
-int alarmValueTop = HIGH;         // Variable to hold the PIR status
-int alarmValueBottom = HIGH;      // Variable to hold the PIR status
-int ledPin = 13;                  // LED on the arduino board flashes when PIR activated
-int LDRSensor = A0;               // Light dependant resistor
-int LDRValue = 0;                 // Variable to hold the LDR value
+
+//-------------------------------------------------------------------------------------------------------
+// TUNING PART: for the Breathe effect, turn-on and turn-off speed and LDR sesonr usage
+//-------------------------------------------------------------------------------------------------------
+int change = 2;                 // used in 'breathing' the LED's , make value smalle to make it smoother, or higher to make it faster
+int breathe = 25;               // used in 'breathing' the LED's.
+int turnOnSpeed = 250;          // speed to turn on next led-strip, in msec between next strip
+int turnOffSpeed = 100;         // speed to turn on next led-strip, in msec between next strip
+int keepLedsOnTime = 18000;     // keep leds on for at least .. msec.
+int keepLedsOffTime = 1500;     // keep leds off for at least .. msec.
 
 
-//-------------------------------------------------------------------------
-// Tuning part for the Breathe effect en turn-on and turn-off speed 
-//-------------------------------------------------------------------------
-int change = 2;                   // used in 'breathing' the LED's , make value smalle to make it smoother, or higher to make it faster
-int breathe = 25;                 // used in 'breathing' the LED's.
-int turnOnSpeed=250;              // speed to turn on next led-strip, in msec between next strip
-int turnOffSpeed=100;             // speed to turn on next led-strip, in msec between next strip
-int keepLedsOnTime=18000;         // keep leds on for at least .. msec.
-int keepLedsOffTime=1500;         // keep leds off for at least .. msec.
 
 void setup() {
 
@@ -69,21 +86,45 @@ void loop() {
   // register the current time (in msec, for later use to optimize the execution loop)
   timeLoopStart=millis();
 
+  // By default read the PIR inputs (unless LDR sensors are used and it is light enough...)
+  readPIRInputs = true;
+  if (useLDR) {
+    // Read the value of the Light Sensor
+    LDRValue = analogRead(LDRSensor);
 
-  alarmValueTop = digitalRead(alarmPinTop); // Constantly poll the PIR at the top of the stairs
+    // For finetuning, show the LDR value, so the LDRThreshold can be determined ; once it works remove the next statement (println)
+    Serial.println(LDRValue); 
+
+    readPIRInputs = false;
+
+    // Only switch on LED's at night when LDR senses low light conditions 
+    if (LDRValue < LDRThreshold) {   
+      // There is enough light, the stairs/ledstips will not be activated
+      readPIRInputs = false;
+    }
+  }
+
+  // Read the PIR inputs 
+  if (readPIRInputs) {
+    alarmValueTop = digitalRead(alarmPinTop); // Constantly poll the PIR at the top of the stairs
+    alarmValueBottom = digitalRead(alarmPinBottom); // Constantly poll the PIR at the bottom of the stairs
+  }
+  
+  // Check if PIR Top was triggered an leds must be turned on
   if ( (alarmValueTop == HIGH) && (downUp == 0) ) { // the 2nd term indicates that there is currently no activity (up or down)
     timeOut=millis(); // Timestamp when the PIR is triggered. The LED cycle wil then start.
     downUp = 1;
     colourWipeDown(strip.Color(255, 255, 250), turnOnSpeed ); // Warm White,led-light from top to down
   }
 
-  alarmValueBottom = digitalRead(alarmPinBottom); // Constantly poll the PIR at the bottom of the stairs
+  // Check if PIR Bottom was triggered an leds must be turned on
   if ( (alarmValueBottom == HIGH) && (downUp == 0)) { // the 2nd term indicates that there is currently no activity (up or down)
     timeOut=millis(); // Timestamp when the PIR is triggered. The LED cycle wil then start.
     downUp = 2;
     colourWipeUp(strip.Color(255, 255, 250), turnOnSpeed); // Warm White,led-light from bottom to top
   }
 
+  // Logic to turn the leds off (determines the turn-off direction)
   if ( (downUp!=0) && (timeOut+keepLedsOnTime < millis()) ) { //switch off LED's in the direction of travel.
     if (downUp == 1) {
       downUp = 3;     // mode = turn off leds from top to down
@@ -97,8 +138,12 @@ void loop() {
     }
   }
 
-  if (downUp==0)            // Currently no activity on the stairs ? (in idle mode, not turned (or turning) on or off ?)     
-    handleBreathe();        // Then Enable the cool "breathe effect" of the led strip lights
+  // Enable Breathe Effect when needed.
+  if (downUp==0) {          // Currently no activity on the stairs ? (in idle mode, not turned (or turning) on or off ?) 
+    if (readPIRInputs) {    // Also the PIR sensors are read (so when LDRs are enabled, then it is dark enough)
+          handleBreathe();  // so....Enable the cool "breathe effect" of the led strip lights  
+    }
+  }
   else if (downUp==5) {     // eventually the stairs led lights will be turned off again (mode=5)
     delay(keepLedsOffTime); // allow small delay/pause and then activate the stairs again with the breathe and motion detection function
     downUp=0;               // set to 0 to allow breathe/motion detection (-1 for debugging one run)
--- README.md
+++ README.md
@@ -15,11 +15,11 @@ Stair LedLights, using NeoPixel Ledstrips and an Arduino.
 ```javascript
 
 // Which pin on the Arduino is connected to the NeoPixels?
-#define PIN             5        // Pin 5 used for the NeoPixels
+#define PIN             5        // Pin used for the NeoPixels
 
 // How many NeoPixels are attached to the Arduino?
-#define LEDSPERSTRIP    36       // 36 leds per strip
-#define LEDSTRIPS       16       // 16 steps
+#define LEDSPERSTRIP    36       // Number of leds per strip
+#define LEDSTRIPS       16       // Number of stair steps (led strips)
 ```
 
 
@@ -28,12 +28,12 @@ Stair LedLights, using NeoPixel Ledstrips and an Arduino.
 //-------------------------------------------------------------------------
 // Tuning part for the Breathe effect en turn-on and turn-off speed 
 //-------------------------------------------------------------------------
-int change = 2;                   // used in 'breathing' the LED's , make value smalle to make it smoother, or higher to make it faster
-int breathe = 25;                 // used in 'breathing' the LED's.
-int turnOnSpeed=250;              // speed to turn on next led-strip, in msec between next strip
-int turnOffSpeed=100;             // speed to turn on next led-strip, in msec between next strip
-int keepLedsOnTime=18000;         // keep leds on for at least .. msec.
-int keepLedsOffTime=1500;         // keep leds off for at least .. msec.
+int change          = 2;         // used in 'breathing' the LED's , make value smalle to make it smoother, or higher to make it faster
+int breathe         = 25;        // used in 'breathing' the LED's.
+int turnOnSpeed     = 250;       // speed to turn on next led-strip, in msec between next strip
+int turnOffSpeed    = 100;       // speed to turn on next led-strip, in msec between next strip
+int keepLedsOnTime  = 18000;     // keep leds on for at least .. msec.
+int keepLedsOffTime = 1500;      // keep leds off for at least .. msec.
 ```
 
 
