20fbafec0cfe8c7998a92a0213ca2b3155074017
Vincent van Beek <vin.van.beek@outlook.com>
2019-01-13 22:35:10 +0100

    Breathe function more generic

--- NeoPixelStairs.ino/NeoPixelStairs.ino.ino
+++ NeoPixelStairs.ino/NeoPixelStairs.ino.ino
@@ -1,26 +1,36 @@
 // NeoPixel Stair Led-strips
-// Simple example by: Vincent van Beek
-// 06-01-2019
+//  
+// 06-01-2019 Vinz68  Initial release     
+// 11-01-2019 Vinz68  LDR sensor added    
+// 13-01-2019 Vinz68  Beathe function more configurable (number of used leds, BREATHELEDS)
 
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
 #include <avr/power.h>
 #endif
 
-
 // Configuration of the NeoPixel output
-#define PIN             5        // Pin used for the NeoPixels
+#define PIN             5         // Pin used for the NeoPixels
 
 // How many NeoPixels are attached to the Arduino ?
-#define LEDSPERSTRIP    36       // Number of leds per strip
-#define LEDSTRIPS       16       // Number of stair steps (led strips)
+#define LEDSPERSTRIP    36        // Number of leds per strip
+#define LEDSTRIPS       16        // Number of stair steps (led strips)
 #define NUMPIXELS       LEDSTRIPS*LEDSPERSTRIP 
+#define BREATHELEDS     1         // Number of leds used in breathe function. 
+                                  // NOTE:  The number indicates the number of Begin Leds and Last leds per strip 
+                                  //        so with setting = "1", the first and last led of the ledstrip would be used  for the breathe function.
+                                  // NOTE:  Minimal value = 0       (= no breathe function)
+                                  //        Maximum value = LEDSPERSTRIP / 2  (= all leds used in the breathe function)
+
+// When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
 
 // Configuration of the Passive Infrared (PIR)
 int alarmPinTop = 10;             // PIR at the top of the stairs
 int alarmPinBottom = 11;          // PIR at the bottom of the stairs
-int alarmValueTop = HIGH;         // Variable to hold the PIR status
-int alarmValueBottom = HIGH;      // Variable to hold the PIR status
+int alarmValueTop = LOW;          // Variable to hold the PIR status
+int alarmValueBottom = LOW;       // Variable to hold the PIR status
 
 // Configuration of the Light dependent resistor (LDR)
 bool useLDR = true;               // flag, when true the program uses the LDR, set to "false" if you don't have a LDR sensor.
@@ -46,22 +56,15 @@ int downUp = 0;                   // main program mode, The possible values are:
                                   //  5   =   All leds just turned off. After short time, automatically mode will be set to "0".
 
 
-// When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
-// Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
-// example for more information on possible values.
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
-
-
 //-------------------------------------------------------------------------------------------------------
 // TUNING PART: for the Breathe effect, turn-on and turn-off speed and LDR sesonr usage
 //-------------------------------------------------------------------------------------------------------
 int change = 2;                 // used in 'breathing' the LED's , make value smalle to make it smoother, or higher to make it faster
 int breathe = 25;               // used in 'breathing' the LED's.
-int turnOnSpeed = 250;          // speed to turn on next led-strip, in msec between next strip
-int turnOffSpeed = 100;         // speed to turn on next led-strip, in msec between next strip
+int turnOnSpeed = 300;          // speed to turn on next led-strip, in msec between next strip
+int turnOffSpeed = 200;         // speed to turn off next led-strip, in msec between next strip
 int keepLedsOnTime = 18000;     // keep leds on for at least .. msec.
 int keepLedsOffTime = 1500;     // keep leds off for at least .. msec.
-
 
 
 void setup() {
@@ -71,7 +74,20 @@ void setup() {
   strip.setBrightness(35);  // Adjust brightness here
   clearStrip();             // Initialize all pixels to 'off', and do strip.show()
   
-  //Serial.begin (9600);    // only required for debugging
+  Serial.begin (9600);      // only required for debugging. Output some settings in the Serial Monitor Window 
+  Serial.print("NeoPixel used on outout-pin [");  
+  Serial.print(PIN); 
+  Serial.print("] with ");  
+  Serial.print(NUMPIXELS ); 
+  Serial.println(" Pixels"); 
+  if (useLDR) {
+    Serial.print("LDR used on analog input pin [");
+    Serial.print(LDRSensor);
+    Serial.println("]");
+    Serial.print("LDR current value =");
+    LDRValue = analogRead(LDRSensor);
+    Serial.println( LDRValue );
+  }
 
   // Configure the used digital input & output
   pinMode(ledPin, OUTPUT); // initilise the onboard pin 13 LED as an indicator
@@ -92,8 +108,8 @@ void loop() {
     // Read the value of the Light Sensor
     LDRValue = analogRead(LDRSensor);
 
-    // For finetuning, show the LDR value, so the LDRThreshold can be determined ; once it works remove the next statement (println)
-    Serial.println(LDRValue); 
+    // For finetuning, show the LDR value, so the LDRThreshold can be determined ; once it works disable the next statement with '//'
+    // Serial.println(LDRValue); 
 
     readPIRInputs = false;
 
@@ -102,12 +118,16 @@ void loop() {
       // There is enough light, the stairs/ledstips will not be activated
       readPIRInputs = false;
     }
+    else {
+      alarmValueTop = LOW;
+      alarmValueBottom = LOW;
+    }
   }
 
   // Read the PIR inputs 
   if (readPIRInputs) {
-    alarmValueTop = digitalRead(alarmPinTop); // Constantly poll the PIR at the top of the stairs
-    alarmValueBottom = digitalRead(alarmPinBottom); // Constantly poll the PIR at the bottom of the stairs
+  //  alarmValueTop = digitalRead(alarmPinTop); // Constantly poll the PIR at the top of the stairs
+  //  alarmValueBottom = digitalRead(alarmPinBottom); // Constantly poll the PIR at the bottom of the stairs
   }
   
   // Check if PIR Top was triggered an leds must be turned on
@@ -141,15 +161,15 @@ void loop() {
   // Enable Breathe Effect when needed.
   if (downUp==0) {          // Currently no activity on the stairs ? (in idle mode, not turned (or turning) on or off ?) 
     if (readPIRInputs) {    // Also the PIR sensors are read (so when LDRs are enabled, then it is dark enough)
-          handleBreathe();  // so....Enable the cool "breathe effect" of the led strip lights  
+      handleBreathe();      // so....Enable the cool "breathe effect" of the led strip lights  
     }
   }
   else if (downUp==5) {     // eventually the stairs led lights will be turned off again (mode=5)
     delay(keepLedsOffTime); // allow small delay/pause and then activate the stairs again with the breathe and motion detection function
     downUp=0;               // set to 0 to allow breathe/motion detection (-1 for debugging one run)
   }
-
 }
+
 
 
 void handleBreathe() {
@@ -175,44 +195,28 @@ void handleBreathe() {
     }
   }
   else {
-    
-    // Set outside leds to breathe-value
-    strip.setPixelColor(0, strip.Color(0,0,breathe));
-    strip.setPixelColor(35, strip.Color(0,0,breathe));
-    strip.setPixelColor(36, strip.Color(0,0,breathe));
-    strip.setPixelColor(71, strip.Color(0,0,breathe));
-    strip.setPixelColor(72, strip.Color(0,0,breathe));
-    strip.setPixelColor(107, strip.Color(0,0,breathe));
-    strip.setPixelColor(108, strip.Color(0,0,breathe));
-    strip.setPixelColor(143, strip.Color(0,0,breathe));
-    strip.setPixelColor(144, strip.Color(0,0,breathe));
-    strip.setPixelColor(179, strip.Color(0,0,breathe));
-    strip.setPixelColor(180, strip.Color(0,0,breathe));
-    strip.setPixelColor(215, strip.Color(0,0,breathe));
-    strip.setPixelColor(216, strip.Color(0,0,breathe));
-    strip.setPixelColor(251, strip.Color(0,0,breathe));
-    strip.setPixelColor(252, strip.Color(0,0,breathe));
-    strip.setPixelColor(287, strip.Color(0,0,breathe));
-    strip.setPixelColor(288, strip.Color(0,0,breathe));
-    strip.setPixelColor(323, strip.Color(0,0,breathe));
-    strip.setPixelColor(324, strip.Color(0,0,breathe));
-    strip.setPixelColor(359, strip.Color(0,0,breathe));
-    strip.setPixelColor(360, strip.Color(0,0,breathe));
-    strip.setPixelColor(395, strip.Color(0,0,breathe));
-    strip.setPixelColor(396, strip.Color(0,0,breathe));
-    strip.setPixelColor(431, strip.Color(0,0,breathe));
-    strip.setPixelColor(432, strip.Color(0,0,breathe));
-    strip.setPixelColor(467, strip.Color(0,0,breathe));
-    strip.setPixelColor(468, strip.Color(0,0,breathe));
-    strip.setPixelColor(503, strip.Color(0,0,breathe));
-    strip.setPixelColor(504, strip.Color(0,0,breathe));
-    strip.setPixelColor(539, strip.Color(0,0,breathe));
-    strip.setPixelColor(540, strip.Color(0,0,breathe));
-    strip.setPixelColor(575, strip.Color(0,0,breathe));
-  
+
+    // For each stair/step, "breathe" the configured number of pixels 
+    for (uint16_t k = 0; k < LEDSTRIPS; k++) {
+      int startFirst = (NUMPIXELS/LEDSTRIPS) * k;
+      int endFirst = startFirst + BREATHELEDS;
+
+      // Turn on first "BREATHELEDS" of each stripm with the breathe color.
+      for (uint16_t i = startFirst; i < endFirst; i++) {
+            strip.setPixelColor(i, strip.Color(0,0,breathe));
+      }
+
+      int startLast = ((NUMPIXELS/LEDSTRIPS) * k) + (LEDSTRIPS - BREATHELEDS) ;
+      int endLast= ((NUMPIXELS/LEDSTRIPS) * k) + BREATHELEDS;
+
+      // Turn on last "BREATHELEDS" of each stripm with the breathe color.
+      for (uint16_t i = startLast; i < endLast; i++) {
+            strip.setPixelColor(i, strip.Color(0,0,breathe));
+      }
+    }
     strip.show();
   
-      timeTemp = millis();
+    timeTemp = millis();
     timeDiff = timeTemp - timeLoopStart;
   
     // Program execution (timeDiff) was shorter than 25 msec ? Then delay up to 25 msec, for a smooth breathe
@@ -233,7 +237,7 @@ void colourWipeDown(uint32_t c, uint16_t wait) {
   for (uint16_t k = 0; k < LEDSTRIPS; k++){
     
     int start = (NUMPIXELS/LEDSTRIPS) *k;
-    Serial.println(k);
+    // Serial.println(k);
 
     for (uint16_t j = start; j < start + LEDSPERSTRIP; j++){
       strip.setPixelColor(j, c);
@@ -250,7 +254,7 @@ void colourWipeUp(uint32_t c, uint16_t wait) {
   for (uint16_t k = LEDSTRIPS; k > 0; k--){
 
     int start = NUMPIXELS/LEDSTRIPS *k;
-    //Serial.println(k);
+    // Serial.println(k);
 
     int x = start;
     do
