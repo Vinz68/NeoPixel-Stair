f36e18f6bb26e876489b8656ac24330a910331f7
Vincent van Beek <vin.van.beek@outlook.com>
2019-01-19 09:54:37 +0100

    Serial.prints from breathe removed , to speed-up

--- NeoPixelStairs.ino/NeoPixelStairs.ino.ino
+++ NeoPixelStairs.ino/NeoPixelStairs.ino.ino
@@ -96,12 +96,19 @@ void setup() {
   }
   if (BREATHELEDS>0) {
         Serial.println("Breathe effect is enabled, on each strip, on pixels:");
-        Serial.print("0 to ");
-        Serial.print(BREATHELEDS);
-        Serial.print(" and ");
-        Serial.print(LEDSPERSTRIP-BREATHELEDS);
-        Serial.print(" up to ");
-        Serial.println(LEDSPERSTRIP-1);
+
+        if (BREATHELEDS>1) { 
+          Serial.print("0 to ");
+          Serial.print(BREATHELEDS-1);
+          Serial.print(" and ");
+          Serial.print(LEDSPERSTRIP-BREATHELEDS);
+          Serial.print(" up to ");
+          Serial.println(LEDSPERSTRIP-1);
+        }
+        else { 
+          Serial.print("0 and ");
+          Serial.println(LEDSPERSTRIP-1);
+        }
   }
   Serial.println("-------------------------------------------------"); 
 
@@ -191,8 +198,6 @@ void handleBreathe() {
   // blue value changes
   breathe = breathe + change;
 
-  Serial.println("handleBreathe"); 
-  
   // breathe the LED from 20 = off to 100 = fairly bright, change values if needed
   if ( (breathe >= 100 || breathe <= 20) ) {
   
@@ -218,7 +223,6 @@ void handleBreathe() {
       int endFirst = startFirst + BREATHELEDS;
 
       // Turn on first "BREATHELEDS" of each stripm with the breathe color.
-      Serial.println("Firsts:"); 
       for (uint16_t i = startFirst; i < endFirst; i++) {
             Serial.println(i); 
             strip.setPixelColor(i, strip.Color(0,0,breathe));
@@ -228,7 +232,6 @@ void handleBreathe() {
       int endLast = startLast + BREATHELEDS;
 
       // Turn on last "BREATHELEDS" of each stripm with the breathe color.
-      Serial.println("Lasts:"); 
       for (uint16_t i = startLast; i < endLast; i++) {
             Serial.println(i); 
             strip.setPixelColor(i, strip.Color(0,0,breathe));
